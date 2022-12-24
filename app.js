bd0d40618cbecb9fbdca3705dd37135a91929e71
Ilham Hermawan <ilhamhermawanzero@gmail.com>
2022-12-24 21:09:13 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,353 @@
+const port = 3000;
+const api_key = "3012151dfasdas3d2022dad215w";
+const qrcodeTerimnal = require('qrcode-terminal');
+var checkReadyWhatsapp = false;
+
+const { Client, MessageMedia, LocalAuth } = require('whatsapp-web.js');
+const express = require('express');
+const { body, validationResult } = require('express-validator');
+const socketIO = require('socket.io');
+const qrcode = require('qrcode');
+const http = require('http');
+const fs = require('fs');
+const { phoneNumberFormatter } = require('./helpers/formatter');
+const axios = require('axios');
+const mime = require('mime-types');
+
+const app = express();
+const server = http.createServer(app);
+
+const io = socketIO(server, {
+  cors: {
+    // origins : ["*"],
+    origin: ["http://localhost","https://localhost","http://localhost:3000","http://wa.mahadigipro.my.id","https://wa.mahadigipro.my.id","http://www.wa.mahadigipro.my.id","https://www.wa.mahadigipro.my.id"],
+    // credentials: true
+    handlePreflightRequest: (req, res) => {
+      res.writeHead(200, {
+        "Access-Control-Allow-Origin": "http://localhost,https://localhost,http://localhost:3000,http://wa.mahadigipro.my.id,https://wa.mahadigipro.my.id,http://www.wa.mahadigipro.my.id,https://www.wa.mahadigipro.my.id",
+        "Access-Control-Allow-Methods": "GET,POST",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Access-Control-Allow-Credentials": true
+      });
+      res.end();
+    }}
+  });
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+//Jika ingin Login QR Lewat Via Web
+
+// app.get('/', (req, res) => {
+//   res.sendFile('index.html', {
+//     root: __dirname
+//   });
+// });
+
+
+const client = new Client({
+  restartOnAuthFail: true,
+  puppeteer: {
+    headless: true,
+    args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-dev-shm-usage',
+    '--disable-accelerated-2d-canvas',
+    '--no-first-run',
+    '--no-zygote',
+      '--single-process', // <- this one doesn't works in Windows
+      '--disable-gpu'
+      ],
+
+    },
+    authStrategy: new LocalAuth()
+  });
+
+client.on('message', msg => {
+  if (msg.body == '!ping') {
+    msg.reply('pong');
+  }
+});
+
+//Jika ingin Login QR Lewat Via Terminal
+
+﻿client.on('qr', qr => {
+  checkReadyWhatsapp = false;
+  qrcodeTerimnal.generate(qr, {small: true});
+  console.log('QR RECEIVED', qr);
+});
+
+client.on('ready', () => {
+  checkReadyWhatsapp = true;
+  console.log('Client is ready!'); 
+});
+
+client.on('authenticated', () => {
+  checkReadyWhatsapp = true;
+  console.log('AUTHENTICATED');
+});
+
+client.on('auth_failure', function(session) {
+  checkReadyWhatsapp = false;
+  console.log('auth_failure');
+});
+
+client.on('disconnected', (session) => {
+  checkReadyWhatsapp = false;
+  client.destroy();
+  client.initialize();
+  console.log('disconnect Whatsapp-bot', session);
+});
+
+client.initialize();
+
+
+// // Socket IO QR Via Web
+// io.on('connection', function(socket) {
+//   socket.emit('message', 'Connecting...');
+//   client.on('qr', (qr) => {
+//     console.log('QR RECEIVED', qr);
+//     qrcode.toDataURL(qr, (err, url) => {
+//       socket.emit('qr', url);
+//       socket.emit('message', 'QR Code received, scan please!');
+//       checkReadyWhatsapp = false;
+//     });
+//   });
+
+//   client.on('ready', () => {
+//     socket.emit('ready', 'Whatsapp is ready!');
+//     socket.emit('message', 'Whatsapp is ready!');
+//     checkReadyWhatsapp = false;
+//   });
+
+//   client.on('authenticated', () => {
+//     socket.emit('authenticated', 'Whatsapp is authenticated!');
+//     socket.emit('message', 'Whatsapp is authenticated!');
+//     console.log('AUTHENTICATED');
+//     checkReadyWhatsapp = true;
+//   });
+
+//   client.on('auth_failure', function(session) {
+//     socket.emit('message', 'Auth failure, restarting...');
+//     checkReadyWhatsapp = false;
+//   });
+
+//   client.on('disconnected', (reason) => {
+//     socket.emit('message', 'Whatsapp is disconnected!');
+//     client.destroy();
+//     client.initialize();
+//     checkReadyWhatsapp = false;
+//   });
+// });
+
+
+const checkRegisteredNumber = async function(number) {
+  const isRegistered = await client.isRegisteredUser(number);
+  return isRegistered;
+}
+
+
+// Send message
+app.post('/check-number', [
+  body('api_key').custom((value, { req }) => {
+    if (value != api_key) {
+      throw new Error('Invalid Api Key');
+    }
+    return true;
+  }),
+  body('number').notEmpty(),
+  ], async (req, res) => {
+    const errors = validationResult(req).formatWith(({
+      msg
+    }) => {
+      return msg;
+    });
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        status: false,
+        message: errors.mapped()
+      });
+    }
+
+    if (!checkReadyWhatsapp) {
+      return res.status(422).json({
+        status: false,
+        message: 'Server Belum Siap'
+      });
+    }
+
+
+    if (req.body.number.length >= 15 || req.body.number.length <= 10) {
+      return res.status(422).json({
+        status: false,
+        message: 'The number above 15 digit or below 10 digit'
+      });
+    }
+
+    const number = phoneNumberFormatter(req.body.number);
+    
+
+    const isRegisteredNumber = await checkRegisteredNumber(number);
+
+    if (!isRegisteredNumber) {
+      return res.status(422).json({
+        status: false,
+        message: 'The number is not registered'
+      });
+    }
+
+    return res.status(200).json({
+      status: true,
+      message: 'The number registered'
+    });
+
+
+    
+  });
+
+
+// Send message
+app.post('/send-message', [
+  body('api_key').custom((value, { req }) => {
+    if (value != api_key) {
+      throw new Error('Invalid Api Key');
+    }
+    return true;
+  }),
+  body('number').notEmpty(),
+  body('message').notEmpty(),
+  ], async (req, res) => {
+    const errors = validationResult(req).formatWith(({
+      msg
+    }) => {
+      return msg;
+    });
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        status: false,
+        message: errors.mapped()
+      });
+    }
+
+    if (!checkReadyWhatsapp) {
+      return res.status(422).json({
+        status: false,
+        message: 'Server Belum Siap'
+      });
+    }
+
+    if (req.body.number.length >= 15 || req.body.number.length <= 10) {
+      return res.status(422).json({
+        status: false,
+        message: 'The number above 15 digit or below 10 digit'
+      });
+    }
+    const number = phoneNumberFormatter(req.body.number);
+    const message = req.body.message;
+    const isRegisteredNumber = await checkRegisteredNumber(number);
+
+    if (!isRegisteredNumber) {
+      return res.status(422).json({
+        status: false,
+        message: 'The number is not registered'
+      });
+    }
+
+    client.sendMessage(number, message).then(response => {
+      res.status(200).json({
+        status: true,
+        response: response
+      });
+    }).catch(err => {
+      res.status(500).json({
+        status: false,
+        response: err
+      });
+    });
+  });
+
+
+const findGroupByName = async function(name) {
+  const group = await client.getChats().then(chats => {
+    return chats.find(chat => 
+      chat.isGroup && chat.name.toLowerCase() == name.toLowerCase()
+      );
+  });
+  return group;
+}
+
+// Send message to group
+// You can use chatID or group name, yea!
+app.post('/send-group-message', [
+  body('api_key').custom((value, { req }) => {
+    if (value != api_key) {
+      throw new Error('Invalid Api Key');
+    }
+    return true;
+  }),
+  body('id').custom((value, { req }) => {
+    if (!value && !req.body.name) {
+      throw new Error('Invalid value, you can use `id` or `name`');
+    }
+    return true;
+  }),
+  body('message').notEmpty(),
+  ], async (req, res) => {
+    const errors = validationResult(req).formatWith(({
+      msg
+    }) => {
+      return msg;
+    });
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        status: false,
+        message: errors.mapped()
+      });
+    }
+
+    if (!checkReadyWhatsapp) {
+      return res.status(422).json({
+        status: false,
+        message: 'Server Belum Siap'
+      });
+    }
+
+    let chatId = req.body.id;
+    const groupName = req.body.name;
+    const message = req.body.message;
+
+  // Find the group by name
+  if (!chatId) {
+    const group = await findGroupByName(groupName);
+    if (!group) {
+      return res.status(422).json({
+        status: false,
+        message: 'No group found with name: ' + groupName
+      });
+    }
+    chatId = group.id._serialized;
+  }
+
+  client.sendMessage(chatId, message).then(response => {
+    res.status(200).json({
+      status: true,
+      response: response
+    });
+  }).catch(err => {
+    res.status(500).json({
+      status: false,
+      response: err
+    });
+  });
+});
+
+
+server.listen(port, function() {
+  console.log('App running on *: ' + port);
+});
---
+++ readme.md
@@ -0,0 +1,130 @@
+#Whatsapp-Api By whatsapp-web.js (wwebjs.dev),
+#Login QR via terminal dan QR via Web
+#Doc : docs.wwebjs.dev
+
+#TUTORIAL INSTALL Node JS di Localhost
+yarn install
+
+node app.js
+
+#TUTORIAL INSTALL NODE JS NGINX PM2 di VPS
+#APT GET UDPDATE DAHULU
+sudo apt-get update
+
+
+#JIKA INGIN GANTI GANTI PORT SSH
+nano /etc/ssh/sshd_config
+
+systemctl restart sshd
+
+
+#PUPETTER
+sudo apt-get install -y gconf-service libasound2 libatk1.0-0 libatk-bridge2.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
+
+sudo apt-get install -y libgbm-dev
+
+
+#INSTALL NODE JS 16.x
+curl -s https://deb.nodesource.com/setup_16.x | sudo bash
+
+sudo apt-get update
+
+sudo apt-get install nodejs -y
+
+sudo apt-get install yarn -y
+
+
+#contoh TESTING PROJECT NODE JS
+cd ~
+
+nano hello.js
+
+#ISI Nano hello.js
+const http = require('http');
+const hostname = 'localhost';
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World!\n');
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+#END Nano
+
+node hello.js
+
+#NGINX
+apt install nginx -y
+
+nano /etc/nginx/sites-available/sites-name.com
+
+#ISI Nano
+server {
+        listen 80;
+        listen [::]:80;
+        root /root;
+        server_name sites-name.com;
+    location / {
+        proxy_pass http://localhost:3000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host $host;
+        proxy_cache_bypass $http_upgrade;
+    }
+}
+#End Nano
+
+ln -s /etc/nginx/sites-available/sites-name.com /etc/nginx/sites-enabled/sites-name.com
+
+nginx -t
+
+systemctl restart nginx
+
+#INSTALL PM2
+npm install pm2@latest -g
+
+pm2 startup systemd
+
+systemctl start pm2-root
+
+#INSTALL WA.ZIP VIA WGET
+cd ~
+
+mkdir wa
+
+cd wa
+
+#wget file menggunkan transfer.sh 
+wget .....
+
+#testing apakah suskses
+node app.js
+
+#jika sukses, jalankan via pm2
+pm2 start app.js
+
+pm2 save
+
+#SSL
+sudo apt install certbot python3-certbot-nginx
+
+sudo certbot --nginx -d sites-name.com
+
+sudo systemctl status certbot.timer
+
+sudo certbot renew --dry-run RENEW
+
+
+#SEKURITI TAMBAHAN UFW, BOLEH DIGUNKAN BOLEH TIDAK
+sudo apt-get install -y ufw
+
+sudo ufw allow 22
+
+sudo ufw allow 3000
+
+sudo ufw allow 'Nginx Full'
+
+sudo ufw enable
